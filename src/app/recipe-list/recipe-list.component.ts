3fdd0fb6159a7906b3a55348efe216598283ba1f
peter heinum <peter.heinum@gmail.com>
2019-02-07 15:36:12 +0100

    added bootstrap and fixed design for cards and stuff

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { RecipeDetailsComponent } from '../recipe-details/recipe-details.component';
 import { RecipeService } from "../recipe.service";
 import { Recipe } from '../recipe';
@@ -12,7 +12,7 @@ import { HttpClient } from "@angular/common/http";
 })
 export class RecipeListComponent implements OnInit {
   recipes: any;
-  recipePictures = [];
+  recipePictures: Recipe[];
 
 
   
@@ -22,13 +22,22 @@ export class RecipeListComponent implements OnInit {
 
     ){}
 
+  handleSearch(event: any){
+    console.log(event.target.value);
+  }
+  
   ngOnInit() {
-    this.yummlyService.getYummlyRecipes("fish soup").subscribe(data => {
-      data.matches.forEach(element => {
-        console.log(element.imageUrlsBySize);
-        this.recipePictures.push(element.imageUrlsBySize[90]);
-      });
-    });
+   this.searchYummly('fish soup');
+  }
+
+  searchYummly(searchQuery){
+    this.recipePictures = this.recipeService.getRecipes();
+    // this.yummlyService.getYummlyRecipes(searchQuery).subscribe(data => {
+    //   data.matches.forEach(element => {
+    //     console.log(element.imageUrlsBySize);
+    //     this.recipePictures.push(element.imageUrlsBySize[90]);
+    //   });
+    // });
   }
 
 }
--- src/app/yummly.service.ts
+++ src/app/yummly.service.ts
@@ -8,8 +8,7 @@ export class YummlyService {
   constructor(private httpclient: HttpClient) { }
   searchString:string;
 
-  appid='bf961eda';
-  appkey='ea969401e668bbbeac4e5c3e91628c73';
+  
 //_app_id=${this.appidappid}&_app_key=${this.appkey}
 
   // getYummlyRecipes = (q:string) => {
@@ -26,14 +25,14 @@ export class YummlyService {
   // }
 
 
-
+  appid='bf961eda';
+  appkey='ea969401e668bbbeac4e5c3e91628c73';
   getYummlyRecipes = (q:string) => {
     q = encodeURIComponent(q.trim())
     this.searchString = q + '&requirePictures=true';
       return this.httpclient.get<any>(`http://api.yummly.com/v1/api/recipes?_app_id=${this.appid}&_app_key=${this.appkey}&q=${this.searchString}`, {
       })
-    }
-  
+    }  
 }
 
 
